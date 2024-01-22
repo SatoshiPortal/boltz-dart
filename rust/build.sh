15ac4ad6d470094ac52ef26c230458919b24426b
i5hi <vishalmenon.92@gmail.com>
2024-01-22 13:28:51 +0530

    Added jniLibs android binaries

--- rust/build.sh
+++ rust/build.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -e
 # Android SDK without Android Studio
 # https://proandroiddev.com/how-to-setup-android-sdk-without-android-studio-6d60d0f2812a
-RUST_REPO="/boltz-dart/rust"
+REPO="/boltz-dart"
 
-cd $RUST_REPO && make android
-cargo build --release
+cd $REPO/rust && make android
+# cargo build --release
 
-TARGET_DIRECTORY="$RUST_REPO/target"
-BUILDS_DIRECTORY="$RUST_REPO/builds"
+TARGET_DIRECTORY="$REPO/rust/target"
+BUILDS_DIRECTORY="$REPO/rust/builds"
 
-rm -rf $BUILDS_DIRECTORY
+# rm -rf $BUILDS_DIRECTORY
+
+ARM64_DIR=$REPO/android/src/main/jniLibs/arm64-v8a
+ARMV7_DIR=$REPO/android/src/main/jniLibs/armeabi-v7a
+I686_DIR=$REPO/android/src/main/jniLibs/x86
+X64_DIR=$REPO/android/src/main/jniLibs/x86_86
+
+mkdir -p $ARM64_DIR
+mkdir -p $ARMV7_DIR
+mkdir -p $I686_DIR
+mkdir -p $X64_DIR
 
 mkdir -p $BUILDS_DIRECTORY/armv7-linux-androideabi
 mkdir -p $BUILDS_DIRECTORY/x86_64-linux-android
 mkdir -p $BUILDS_DIRECTORY/aarch64-linux-android
 mkdir -p $BUILDS_DIRECTORY/i686-linux-android
-mkdir -p $BUILDS_DIRECTORY/x86_64-apple-darwin
-mkdir -p $BUILDS_DIRECTORY/x86_64-linux-unknown
+# mkdir -p $BUILDS_DIRECTORY/x86_64-apple-darwin
+# mkdir -p $BUILDS_DIRECTORY/x86_64-linux-unknown
 
-cp $TARGET_DIRECTORY/aarch64-linux-android/release/libboltz_dart.so $BUILDS_DIRECTORY/aarch64-linux-android/
-cp $TARGET_DIRECTORY/x86_64-linux-android/release/libboltz_dart.so $BUILDS_DIRECTORY/x86_64-linux-android/
-cp $TARGET_DIRECTORY/i686-linux-android/release/libboltz_dart.so $BUILDS_DIRECTORY/i686-linux-android/
-cp $TARGET_DIRECTORY/armv7-linux-androideabi/release/libboltz_dart.so $BUILDS_DIRECTORY/armv7-linux-androideabi/
-cp $TARGET_DIRECTORY/release/libboltz_dart.so $BUILDS_DIRECTORY/x86_64-linux-unknown/
+cp $TARGET_DIRECTORY/aarch64-linux-android/release/libboltz_dart.so $ARM64_DIR/
+cp $TARGET_DIRECTORY/x86_64-linux-android/release/libboltz_dart.so $X64_DIR
+cp $TARGET_DIRECTORY/i686-linux-android/release/libboltz_dart.so $I686_DIR
+cp $TARGET_DIRECTORY/armv7-linux-androideabi/release/libboltz_dart.so $ARMV7_DIR
+# cp $TARGET_DIRECTORY/release/libboltz_dart.so $BUILDS_DIRECTORY/x86_64-linux-unknown/
 
 exit
