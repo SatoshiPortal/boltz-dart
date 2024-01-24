1e43eee16944e0803346243bb82da63554591851
mocodesmo <jmorteza@hotmail.com>
2024-01-24 21:05:02 +0530

    init flutter plugin

---
+++ lib/boltz_dart.dart
@@ -0,0 +1,33 @@
+// You have generated a new plugin project without specifying the `--platforms`
+// flag. An FFI plugin project that supports no platforms is generated.
+// To add platforms, run `flutter create -t plugin_ffi --platforms <platforms> .`
+// in this directory. You can also find a detailed instruction on how to
+// add platforms in the `pubspec.yaml` at
+// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:isolate';
+
+import 'boltz_dart_bindings_generated.dart';
+
+const String _libName = 'boltz_dart';
+
+/// The dynamic library in which the symbols for [BoltzDartBindings] can be found.
+final DynamicLibrary _dylib = () {
+  if (Platform.isMacOS || Platform.isIOS) {
+    return DynamicLibrary.open('$_libName.framework/$_libName');
+  }
+  if (Platform.isAndroid || Platform.isLinux) {
+    return DynamicLibrary.open('lib$_libName.so');
+  }
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('$_libName.dll');
+  }
+  throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+}();
+
+/// The bindings to the native functions in [_dylib].
+// final BoltzDartBindings _bindings = BoltzDartBindings(_dylib);
+
