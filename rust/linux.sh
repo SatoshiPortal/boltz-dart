1af5bd0031d295deebc7df755b2e86c607cf3155
Anorak <anorak@pop-os.localdomain>
2024-02-02 18:46:11 +0530

    linux.sh:correction

--- rust/linux.sh
+++ rust/linux.sh
@@ -8,13 +8,13 @@ LINUX_DIR=$ROOT/$NAME.$VERSION/linux # final binaries stored here
 # armv7-unknown-linux-gnueabi \
 # i686-unknown-linux-gnu \
 
-cd boltz-dart
-
-#cd ./src || exit
+cd boltz-dart || exit
 
 for TARGET in \
-    x86_64-unknown-linux-gnu
+    x86_64-unknown-linux-gnu 
+    # aarch64-unknown-linux-gnu install using docker image amd-64/rust:slim-bullseye requires aarch64-linux-gnu-gcc
+
 do
     rustup target add $TARGET
-    cargo build --target-dir $LINUX_DIR -r --target=x86_64-unknown-linux-gnu
+    cargo build --target-dir $LINUX_DIR -r --target=$TARGET
 done
