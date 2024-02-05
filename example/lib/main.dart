b65e6b6780d65190381b8ef8fa79eb01bc8015a8
i5hi <vishalmenon.92@gmail.com>
2024-02-05 11:38:28 +0530

    Remvoed bdk flutter from example. Stripped example to only check for fees.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,26 +1,8 @@
-// import 'package:boltz_dart/boltz_dart.dart' as boltz;
-// import 'package:bdk_flutter/bdk_flutter.dart';
-import 'wallet.dart';
-import 'menu.dart';
+import 'package:boltz_dart/boltz_dart.dart';
 
 void main() async {
-  // create bdk wallet
-  // fund bdk wallet
-  // create submarine swap
-  // pay swap script
-  // check status
-  // create reverse swap
-  // pay invoice (outside app)
-  // check status
-  // claim swap script
-  // check bdk wallet balance
-  // print("Boltz/BDK Demo");
-  // final chain = boltz.Chain.LiquidTestnet;
-  // print('Setting up Boltz/BDK Demo');
-  // print('1. Setting up blockchain');
-  // Blockchain blockchain = await setupBlockchain();
-  // print('2. Setting up wallet');
-  // await setupWallet(blockchain);
-  Menu menu = Menu();
-  menu.displayMenu();
+  const boltzUrl = 'https://api.testnet.boltz.exchange';
+  final amount = 100000;
+  final fees = await AllSwapFees.estimateFee(boltzUrl: boltzUrl, outputAmount: amount);
+  print('${fees.lbtcSubmarine.lockupFeesEstimate}');
 }
