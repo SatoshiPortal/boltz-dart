d3dc0cfb5dafdf74698a2ea3d532d7808ed071ed
i5hi <vishalmenon.92@gmail.com>
2024-02-06 14:09:54 +0530

    Match generated code to binary release

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,5 +4,5 @@ void main() async {
   const boltzUrl = 'https://api.testnet.boltz.exchange';
   final amount = 100000;
   final fees = await AllSwapFees.estimateFee(boltzUrl: boltzUrl, outputAmount: amount);
-  print('${fees.lbtcSubmarine.lockupFeesEstimate}');
+  print('FEES:${fees.lbtcSubmarine.lockupFeesEstimate}');
 }
