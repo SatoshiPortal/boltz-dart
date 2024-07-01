440819678f28c0fd17128652984508566155d05c
saiy2k <saiy2k@gmail.com>
2024-07-01 16:49:48 +0530

    Fees tests for submarine and reverse

--- rust/src/api/fees.rs
+++ rust/src/api/fees.rs
@@ -32,3 +32,69 @@ impl Fees {
         Ok(chain_fees)
     }
 }
+
+// Cross test values by calling the API endpoint 
+// GET https://api.boltz.exchange/v2/swap/submarine
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_fees() {
+        let url = "https://api.boltz.exchange/v2";
+        let fees = Fees::new(url.to_string());
+        assert_eq!(fees.boltz_url, url);
+
+        let url1 = "api.boltz.exchange";
+        let fees1 = Fees::new(url1.to_string());
+        assert_eq!(fees1.boltz_url, "https://".to_owned() + url1);
+    }
+
+    #[test]
+    fn test_submarine_fees() {
+        let url = "https://api.boltz.exchange/v2";
+        let fees = Fees::new(url.to_string());
+
+        let result = fees.submarine();
+        if let Ok(sub_fees) = result {
+            println!("BTC");
+            println!("percentage: {:?}", sub_fees.btc_fees.percentage);
+            println!("miner fees: {:?}", sub_fees.btc_fees.miner_fees);
+            println!("L-BTC");
+            println!("percentage: {:?}", sub_fees.lbtc_fees.percentage);
+            println!("miner fees: {:?}", sub_fees.lbtc_fees.miner_fees);
+        }
+    }
+
+    #[test]
+    fn test_reverse_fees() {
+        let url = "https://api.boltz.exchange/v2";
+        let fees = Fees::new(url.to_string());
+
+        let result = fees.reverse();
+        if let Ok(reverse_fees) = result {
+            println!("BTC");
+            println!("percentage: {:?}", reverse_fees.btc_fees.percentage);
+            println!("miner fees: {:?}", reverse_fees.btc_fees.miner_fees);
+            println!("L-BTC");
+            println!("percentage: {:?}", reverse_fees.lbtc_fees.percentage);
+            println!("miner fees: {:?}", reverse_fees.lbtc_fees.miner_fees);
+        }
+    }
+    
+    /*
+    #[test]
+    fn test_chain_fees() {
+        let url = "https://api.boltz.exchange/v2";
+        let fees = Fees::new(url.to_string());
+
+        let result = fees.chain();
+        if let Ok(chain_fees) = result {
+            println!("BTC");
+            println!("{:?}", chain_fees);
+        }
+    }
+    */
+
+
+}
