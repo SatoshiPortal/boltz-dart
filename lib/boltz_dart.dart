e413d7ae06a3b365d03ed54b8cdaf90359bfb2f5
saiy2k <saiy2k@gmail.com>
2024-07-01 17:37:54 +0530

    exported Fees from lib/boltz_dart.dart

--- lib/boltz_dart.dart
+++ lib/boltz_dart.dart
@@ -5,6 +5,7 @@ export './src/generated/api/btc_ln.dart';
 export './src/generated/api/lbtc_ln.dart';
 export './src/generated/api/error.dart';
 export './src/generated/api/types.dart';
+export './src/generated/api/fees.dart';
 export './src/root.dart';
 export './src/utils/loader.dart';
 export './src/utils/http.dart';
