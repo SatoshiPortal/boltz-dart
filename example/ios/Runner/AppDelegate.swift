c47c3b2323ef9447288f2374c8e8862b8da61782
Vishal Menon <vishalmenon.92@gmail.com>
2024-08-22 17:02:51 +0530

    Updated example ios

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,7 @@
 import Flutter
 import UIKit
 
-<<<<<<< HEAD
-@main
-=======
-// let dummy = dummy_method_to_enforce_bundling()
-// print(dummy)
 @UIApplicationMain
->>>>>>> trunk
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
