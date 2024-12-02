e5449885d335b0393efe9d20e780adf081b1fe9b
ethicnology <ethicnology@pm.me>
2024-12-02 14:44:13 -0500

    docs: improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,62 @@
-import 'package:boltz_dart/boltz_dart.dart' as bltz;
+import 'package:boltz_dart/boltz_dart.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  await test();
-  runApp(MaterialApp(home: Scaffold(body: Container(color: Colors.red))));
+Future<String> fetchFees() async {
+  try {
+    const boltzUrl = 'https://api.testnet.boltz.exchange/v2';
+    final fees = await const Fees(boltzUrl: boltzUrl).chain();
+    print(fees);
+    return fees.toString();
+  } catch (e) {
+    return 'Error fetching fees: $e';
+  }
 }
 
-Future test() async {
-  try {
-    await bltz.LibBoltz.init();
-    const boltzUrl = 'https://api.testnet.boltz.exchange/v2';
-    // const amount = 100000;
-    final fees = await const bltz.Fees(boltzUrl: boltzUrl).chain();
-    print('FEES:${fees}');
-  } catch (e) {
-    print('\n\nERRRR: ' + e.toString() + '\n\n');
+Future<void> main() async {
+  await LibBoltz.init(); // Mandatory initialization
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boltz Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FeesScreen(),
+    );
+  }
+}
+
+class FeesScreen extends StatefulWidget {
+  const FeesScreen({super.key});
+
+  @override
+  State<FeesScreen> createState() => _FeesScreenState();
+}
+
+class _FeesScreenState extends State<FeesScreen> {
+  String _fees = 'Fetching fees...';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFees();
+  }
+
+  Future<void> _loadFees() async {
+    _fees = await fetchFees();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Boltz Fees')),
+      body: Center(child: Text(_fees)),
+    );
   }
 }
