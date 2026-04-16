089774dab26fbc06d93982375b83343c7624df78
ethicnology <ethicnology@pm.me>
2026-04-16 10:59:19 +0300

    feat: add bull_sdk feature to cfg-gate frb_generated
    When used as dependency of bull_sdk, frb_generated is disabled to
    avoid duplicate IntoIntoDart trait impls. Added lib to crate-type
    for rlib output.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod api;
+#[cfg(not(feature = "bull_sdk"))]
 mod frb_generated;
 pub mod util; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
